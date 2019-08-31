7038425859eb461a67e70f43c93acebb78dd8970
xws <xws>
2019-08-31 23:48:17 +0800

    路由系统

--- pro_flask/app.py
+++ pro_flask/app.py
@@ -6,19 +6,25 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import session
+from flask import url_for
 
 
 app=Flask(__name__,template_folder="templates",static_folder="static")
 
 
 # flask 中所有的默认配置文件
-print(app.config)
+# print(app.config)
 
 # app.config["DEBUG"]=True
+# 引入配置文件
 app.config.from_object("settings.DevelopmentConfig")
 
 
-
+@app.route("/index/<int:nid>",methods=["GET","POST"])
+# endpoint="n1" 代指 /index，若不定义 endpoint，则默认值为 函数名 index
+def index(nid):
+    print(url_for("index",nid=777))
+    return "Index"
 
 
 
